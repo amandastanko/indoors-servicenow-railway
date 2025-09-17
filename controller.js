bab1417d5bc269c113a9e039ff69bc96a1fe4ce0
Amanda Stanko <astanko77@gmail.com>
2025-09-17 15:23:54 -0700

    Add files via upload

---
+++ controller.js
@@ -0,0 +1,28 @@
+/* Copyright 2019 Esri
+ *
+ * Licensed under the Apache License Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
+  controller.js
+
+  This file is not required unless additional routes specified in routes.js
+  If so, corresponding functions must be written to match those routes.
+
+  Documentation: http://koopjs.github.io/docs/usage/provider
+*/
+
+function Controller (model) {
+  this.model = model
+}
+
+module.exports = Controller
