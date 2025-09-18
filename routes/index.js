17e867207a336ff2700d9e2acc01467b2761e100
Amanda Stanko <astanko77@gmail.com>
2025-09-18 10:48:41 -0700

    Update routes/index.js
    update

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 
+
 module.exports = [
-  {
-    path: 'locations',
-    methods: ['get'],
-    handler: controller.getLocations
-  }
+  { path: 'incidents', methods: ['get'], handler: controller.getIncidents },
+  { path: 'requests', methods: ['get'], handler: controller.getRequests },
+  { path: 'locations', methods: ['get'], handler: controller.getLocations }
 ];
+
